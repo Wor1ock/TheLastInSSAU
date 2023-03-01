76565a043fbe6d3c2a668b59225ec90f9b483fb1
Iakivw <werutin@gmail.com>
2023-03-01 15:01:43 +0400

    Reworked player movement animation

--- scenes/MainCharacter.gd
+++ scenes/MainCharacter.gd
@@ -16,7 +16,7 @@ var _state = "front"
 
 func _physics_process(_delta):
 	movement_control()
-	animation_control()
+	animation_control(direction)
 
 # Функция для управления движением
 func movement_control():
@@ -25,28 +25,48 @@ func movement_control():
 	# Функция, осуществляющая перемещение
 	direction = move_and_slide(direction * speed)
 	
-func animation_control():
-	if Input.is_action_pressed("ui_right"):
-		_animated_sprite.play("walk_right")
-		_animated_sprite.flip_h = false
-		_state = "right"
-	elif Input.is_action_pressed("ui_left"):
-		_animated_sprite.play("walk_right")
-		_animated_sprite.flip_h = true
-		_state = "left"
-	elif Input.is_action_pressed("ui_up"):
-		_animated_sprite.play("walk_back")
-		_state = "back"
-	elif Input.is_action_pressed("ui_down"):
-		_animated_sprite.play("walk_front")
-		_state = "front"
-	elif Input.is_action_pressed("ui_cancel"):
-		_animated_sprite.play("death_right")
+# Функция для всех анимаций
+# warning-ignore:shadowed_variable
+func animation_control(direction):
+	# Анимация движения в зависимости от направления вектора
+	var _angle = direction.angle()
+	var _length = direction.length()
+	if (_length == 0):
+		match _state:
+			"back":
+				_animated_sprite.play("idle_back")
+			"right":
+				_animated_sprite.play("idle_right")
+				_animated_sprite.flip_h = false
+			"left":
+				_animated_sprite.play("idle_right")
+				_animated_sprite.flip_h = true
+			_:
+				_animated_sprite.play("idle_front")
+	else:
+		if _angle >= -PI/4 and _angle <= PI/4:
+			_animated_sprite.play("walk_right")
+			_animated_sprite.flip_h = false
+			_state = "right"
+		elif _angle > PI/4 and _angle <= 3 * PI/4:
+			_animated_sprite.play("walk_front")
+			_state = "front"
+		elif _angle > 3 * PI/4 and _angle <= 5 * PI/4:
+			_animated_sprite.play("walk_right")
+			_animated_sprite.flip_h = true
+			_state = "left"
+		elif _angle > - 3 * PI/4 and _angle < PI/4:
+			_animated_sprite.play("walk_back")
+			_state = "back"
+			
+	#Анимация действий (атака и смерть)
+	if Input.is_action_pressed("ui_cancel"):
 		match _state:
 			"right":
 				_animated_sprite.flip_h = false
 			"left":
 				_animated_sprite.flip_h = true
+		_animated_sprite.play("death_right")
 	elif Input.is_action_pressed("ui_accept"):
 		match _state:
 			"front":
@@ -58,17 +78,5 @@ func animation_control():
 				_animated_sprite.flip_h = false
 			"left":
 				_animated_sprite.play("attack_right")
-				_animated_sprite.flip_h = true
-	else:
-		match _state:
-			"front":
-				_animated_sprite.play("idle_front")
-			"back":
-				_animated_sprite.play("idle_back")
-			"right":
-				_animated_sprite.play("idle_right")
-				_animated_sprite.flip_h = false
-			"left":
-				_animated_sprite.play("idle_right")
 				_animated_sprite.flip_h = true
 				
