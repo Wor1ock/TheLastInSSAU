bc83835de4a953c8357bd49421ac769f4c56e2eb
lykova1 <e.lykova03@gmail.com>
2023-03-16 14:14:38 +0400

    сделала двух постоянно преследующих игрока врагов

---
+++ scenes/Enemy3.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+onready var player_detection_zone = $PlayerDetectionZone
+onready var patrol_timer = $PatrolTimer
+
+# Переменные для ENGAGE
+var speed = 120
+var motion = Vector2.ZERO
+var player = null
+
+
+# Функция определяет поведение врага в разных состояниях
+func _physics_process(delta):
+	motion = Vector2.ZERO
+	if player:
+		motion = position.direction_to(player.position)*speed
+	motion = move_and_slide(motion)
+	
+	
+func _on_PlayerDetectionZone_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		player = body
+		print("enter")
+
+
+
+
+func _on_HurtboxShape_area_entered(area):
+	queue_free()
