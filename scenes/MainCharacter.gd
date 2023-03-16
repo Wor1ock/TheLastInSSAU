32389b6d269d8a6e093fc50eb26b0f284d4cd0fd
Iakivw <werutin@gmail.com>
2023-03-16 18:22:13 +0400

    Нормальная анимация игрока

--- scenes/MainCharacter.gd
+++ scenes/MainCharacter.gd
@@ -1,82 +1,58 @@
 extends KinematicBody2D
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
 # Movement speed in pixels per second.
 export var speed := 5
 
 var direction := Vector2.ZERO
+var state = MOVE
 
-onready var _animated_sprite = $AnimatedSprite
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
-var _state = "front"
+func _ready():
+	animationTree.active = true
 
 # Про анимирование всего этого добра можно почитать тут:
 # https://www.gdquest.com/tutorial/godot/2d/top-down-movement/
 # Раздел Top-down movement
 
-
 func _physics_process(_delta):
-	movement_control()
-	animation_control(direction)
+	match state:
+		MOVE:
+			movement_control()
+		ATTACK:
+			attack_state()
+		ROLL:
+			pass
 
-# Функция для управления движением
+# Функция для управления движением и анимацией
 func movement_control():
 	# Направление движения персонажа
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", direction)
+		animationTree.set("parameters/Run/blend_position", direction)
+		animationTree.set("parameters/Attack/blend_position", direction)
+		animationState.travel("Run")
+	else:
+		animationState.travel("Idle")
 	# Функция, осуществляющая перемещение
 	direction = move_and_slide(direction * speed)
 	
-# Функция для всех анимаций
-# warning-ignore:shadowed_variable
-func animation_control(direction):
-	# Анимация движения в зависимости от направления вектора
-	var _angle = direction.angle()
-	var _length = direction.length()
-	if (_length == 0):
-		match _state:
-			"back":
-				_animated_sprite.play("idle_back")
-			"right":
-				_animated_sprite.play("idle_right")
-				_animated_sprite.flip_h = false
-			"left":
-				_animated_sprite.play("idle_right")
-				_animated_sprite.flip_h = true
-			_:
-				_animated_sprite.play("idle_front")
-	else:
-		if _angle >= -PI/4 and _angle <= PI/4:
-			_animated_sprite.play("walk_right")
-			_animated_sprite.flip_h = false
-			_state = "right"
-		elif _angle > PI/4 and _angle <= 3 * PI/4:
-			_animated_sprite.play("walk_front")
-			_state = "front"
-		elif _angle > 3 * PI/4 and _angle <= 5 * PI/4:
-			_animated_sprite.play("walk_right")
-			_animated_sprite.flip_h = true
-			_state = "left"
-		elif _angle > - 3 * PI/4 and _angle < PI/4:
-			_animated_sprite.play("walk_back")
-			_state = "back"
-			
-	#Анимация действий (атака и смерть)
-	if Input.is_action_pressed("ui_cancel"):
-		match _state:
-			"right":
-				_animated_sprite.flip_h = false
-			"left":
-				_animated_sprite.flip_h = true
-		_animated_sprite.play("death_right")
-	elif Input.is_action_pressed("ui_accept"):
-		match _state:
-			"front":
-				_animated_sprite.play("attack_front")
-			"back":
-				_animated_sprite.play("attack_back")
-			"right":
-				_animated_sprite.play("attack_right")
-				_animated_sprite.flip_h = false
-			"left":
-				_animated_sprite.play("attack_right")
-				_animated_sprite.flip_h = true
-				
+	if Input.is_action_just_pressed("ui_accept"):
+		state = ATTACK
+
+# Функция для контроля аттаки
+func attack_state():
+	animationState.travel("Attack")
+	
+func attack_animation_finished():
+	state = MOVE
