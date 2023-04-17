edd28ca70b92e31caf4224be8154e9b9c0071f23
spireneko <spireneko@gmail.com>
2023-04-17 16:56:06 +0400

    HP bar полностью работает, добавлено пустое полусердце

--- scenes/UI/HeartsBar.gd
+++ scenes/UI/HeartsBar.gd
@@ -5,6 +5,7 @@ var max_half_hearts := 4 setget _set_max_half_hearts
 var half_hearts := max_half_hearts setget _set_half_hearts
 
 
+onready var textureHalfEmptyHeart = $TextureHalfEmptyHeart
 onready var textureEmptyHeart = $TextureEmptyHeart
 onready var textureHalfHeart = $TextureHalfHeart
 onready var textureFullHeart = $TextureFullHeart
@@ -13,11 +14,14 @@ onready var heart_texture_x_size = textureFullHeart.texture.get_width()
 
 func _set_half_hearts(value):
 	half_hearts = value
-	if textureFullHeart != null:
-		textureFullHeart.rect_size.x = (half_hearts / 2) * heart_texture_x_size
-		print(half_hearts / 2)
-		textureHalfHeart.rect_size.x = ((half_hearts + 1) / 2) * heart_texture_x_size
+	textureFullHeart.rect_size.x = (half_hearts / 2) * heart_texture_x_size
+	textureHalfHeart.rect_size.x = ((half_hearts + 1) / 2) * heart_texture_x_size
 
 func _set_max_half_hearts(value):
 	max_half_hearts = value
 	textureEmptyHeart.rect_size.x = (max_half_hearts / 2) * heart_texture_x_size
+	textureHalfEmptyHeart.margin_left = (max_half_hearts / 2) * heart_texture_x_size
+	if max_half_hearts % 2 == 0:
+		textureHalfEmptyHeart.visible = false
+	else:
+		textureHalfEmptyHeart.visible = true
