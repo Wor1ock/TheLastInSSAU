561e494380d65e08fc82f1ceebaa2139455334ad
Iakivw <werutin@gmail.com>
2023-05-03 00:15:33 +0400

    CloseCombat added

--- scenes/entity/enemy/CloseCombat/CloseCombat.gd
+++ scenes/entity/enemy/CloseCombat/CloseCombat.gd
@@ -3,13 +3,25 @@ extends "res://scenes/entity/enemy/EnemyBase.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animationTree.active = true
 
 onready var attack_timer = $AttackTimer
+
+onready var animationTree = $AnimationTree
 
 
 var victim
 var attack = false
+
+func engage(player):
+	motion = Vector2.ZERO
+	if player and is_instance_valid(player):
+		#look_at(player.position)
+		motion = position.direction_to(player.position)*speed
+		
+	animationTree.set("parameters/blend_position", motion)
+	
+	motion = move_and_slide(motion)
 
 func _on_HitboxShape_area_entered(body):
 	if body.has_method("hurtbox_take_damage"):
