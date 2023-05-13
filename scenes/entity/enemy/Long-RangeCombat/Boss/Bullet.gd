2876d5c2fd512d2453ad6068b9d1f79390eeb0a6
lykova1 <e.lykova03@gmail.com>
2023-05-13 22:26:57 +0400

    добавлен босс

---
+++ scenes/entity/enemy/Long-RangeCombat/Boss/Bullet.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+const RIGHT = Vector2.RIGHT
+export(int) var SPEED: int = 200
+export(int) var damage: int = 1
+
+func _physics_process(delta):
+	var movement = RIGHT.rotated(rotation) * SPEED * delta
+	global_position += movement
+
+func destroy():
+	queue_free()
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+func _on_Bullet_area_entered(area):
+	if area.has_method("hurtbox_take_damage"):
+		area.hurtbox_take_damage(damage)
+		destroy()
